aa3c3261c43b6f7b506ea80f3b3b8dccb86e23a5
Melf <cqc@melf.de>
2026-01-29 15:39:07 +0000

    try to mutable

--- pytket/extensions/qiskit/qiskit_convert.py
+++ pytket/extensions/qiskit/qiskit_convert.py
@@ -1035,7 +1035,8 @@ order or only one bit of one register"""
         if len(qargs) == 2:  # noqa: PLR2004
             z_gate = qiskit_gates.CZGate()
         else:
-            z_gate = qiskit_gates.ZGate().control(len(qargs) - 1)
+            z_gate_ = qiskit_gates.ZGate().control(len(qargs) - 1)
+            z_gate = z_gate_.to_mutable()
             z_gate.name = "mcz"
         _apply_qiskit_instruction(
             qcirc=qcirc, instruc=z_gate, qargs=qargs, condition=condition
